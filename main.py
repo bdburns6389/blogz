726d64704505c5c7934534b3e92167bc81b85874
Brian Burns <bdburns6389@gmail.com>
2017-12-13 21:32:15 -0600

    tidy up

--- main.py
+++ main.py
@@ -44,9 +44,9 @@ def signup():
 def newpost():
     #Need to add blog.owner to this route, but is it a hidden value?
     blog_owner = User.query.filter_by(username=session['username']).first() 
-    '''This will need to relate to
-    SESSIONS before it will work.  Make sure to use username (similar to email in the get it done video)
-    Will also need to be put in like blog_owner=blog_owner in a .query somewhere.'''
+    #This will need to relate toSESSIONS before it will work.
+    # Make sure to use username (similar to email in the get it done video)
+    #Will also need to be put in like blog_owner=blog_owner in a .query somewhere.
     title_error = ""
     body_error = ""
     if request.method == 'POST':
